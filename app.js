36e7c75ad2c86a0462f37a169ded80fd0999c30b
Amit Goren <amitgoren12@gmail.com>
2020-03-13 16:10:56 +0200

    Changed the "/img" URL to "/pictures".

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/img', express.static(config.pictures_dir));
+app.use('/pictures/', express.static(config.pictures_dir));
 
 app.use('/', indexRouter);
 app.use('/vote/', voteRouter);
