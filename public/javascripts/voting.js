14515c245d2346e55a7f7bf6431954479c266fbf
Amit Goren <amitgoren12@gmail.com>
2020-03-14 11:32:41 +0200

    Changed /pictures/ to /i/.

--- public/javascripts/voting.js
+++ public/javascripts/voting.js
@@ -1,5 +1,7 @@
 const queryString = window.location.search;
 
-function vote(preference) {
-  window.location.replace("/prefer"+ queryString + "&preference=" + preference);
+onload = function() {
+  function vote(preference) {
+    window.location.replace("/prefer"+ queryString + "&preference=" + preference);
+  }
 }
